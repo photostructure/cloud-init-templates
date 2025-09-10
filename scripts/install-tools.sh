18b2d42fcba68f5f38b033146f8fee948336b6ac
Matthew McEachen <matthew@photostructure.com>
2025-09-10 11:26:18 -0700

    refactor: remove uv installation step from install-tools.sh

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# Install uv if not present. Will fail if curl is not installed
-command -v uv >/dev/null 2>&1 || curl -LsSf https://astral.sh/uv/install.sh | sh
-
 # Install Go tools. Will fail fast if Go not installed
 command -v yamlfmt >/dev/null 2>&1 || go install github.com/google/yamlfmt/cmd/yamlfmt@latest
 command -v shfmt >/dev/null 2>&1 || go install mvdan.cc/sh/v3/cmd/shfmt@latest
